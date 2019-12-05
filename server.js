7b878d181cc2a0517359da8d2d73e216a358f1ea
Kate <h34rt8e4t@gmail.com>
2019-12-05 19:06:59 +0200

    Description of changes

---
+++ server.js
@@ -0,0 +1,21 @@
+function test1 (form) {
+	if (form.text1.value == '') {
+		alert('Не залишайте поле пустим!')
+	} else {
+		alert('Hi, ' + form.text1.value + '! Форму заповнено коректно!')
+	}
+}
+function test2 (form) {
+	if (form.text2.value == '' || form.text2.value.indexOf('@', 0) == -1) {
+		alert('Неправильно введена адреса e-mail!')
+	} else {
+		alert('OK!')
+	}
+}
+function test3 (input) {
+	if (isNaN(Number(input))) {
+		alert('Номер не коректний.')
+	} else {
+		alert('Номер коректний!')
+	}
+}
